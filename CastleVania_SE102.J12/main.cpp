f462a9699dc2c5962b87f4bde45937a524a717cd
DESKTOP-BAKG7GD\PhucP <phucphan6666@gmail.com>
2018-10-12 07:26:10 +0700

    first commit

---
+++ CastleVania_SE102.J12/main.cpp
@@ -0,0 +1,23 @@
+/* =============================================================
+		INTRODUCTION TO GAME PROGRAMMING SE102.J12
+
+		Game Name: Castle Vania
+
+================================================================ */
+
+#include <windows.h>
+
+#define WINDOW_CLASS_NAME L"SampleWindow"
+#define MAIN_WINDOW_TITLE L"Castle Vania"
+
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(255, 0, 0)
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 640
+
+#define MAX_FRAME_RATE 10
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
